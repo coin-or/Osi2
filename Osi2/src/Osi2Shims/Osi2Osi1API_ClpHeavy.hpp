0228ead3e73c993ccfffd6f7a796f4eb98583dc6
Matthew Saltzman <mjs@clemson.edu>
2011-10-26 02:18:22 +0000

    Virtual destructor.

--- Osi2/src/Osi2Shims/Osi2Osi1API_ClpHeavy.hpp
+++ Osi2/src/Osi2Shims/Osi2Osi1API_ClpHeavy.hpp
@@ -24,7 +24,7 @@ public:
   Osi1API_ClpHeavy () ;
 
   /// Destructor
-  ~Osi1API_ClpHeavy () ;
+  virtual ~Osi1API_ClpHeavy () ;
 
   inline void initialSolve() { OsiClpSolverInterface::initialSolve() ; }
 
